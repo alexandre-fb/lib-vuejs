ce16d6800e6703dbb7726a902f301a130f416555
alexandre-fb <alexandrefb.86@gmail.com>
2024-01-07 14:29:45 -0300

    feat: add content

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,14 @@
 <template>
-  <h2>Footer</h2>
+  <div class="p-2">
+    <h6 class="text-sm text-quaternary">
+      <span class="text-quaternary">
+        Desenvolvido por 👉 
+      </span>
+      <a href="https://www.linkedin.com/in/alexandre-fb/" target="_blank" class="text-inherit transition hover:text-primary">
+        Alexandre Bisognin
+      </a>
+    </h6>
+  </div>
 </template>
 
 <script>
