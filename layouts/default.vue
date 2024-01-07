474854d4300d852fb080533f260a9163cc56e32e
alexandre-fb <alexandrefb.86@gmail.com>
2024-01-07 14:03:02 -0300

    Refactor

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,26 @@
 <template>
-    <div class="flex flex-col justify-start items-center min-h-screen pt-[64px]">
-      <HeaderEsqueleto />
-      <!-- <Header /> -->
-      <main class="w-full">
-        <slot />
-      </main>
-      <Footer />
-    </div>
-  </template>
+  <div class="flex flex-col justify-start items-center min-h-screen">
+    <!-- <HeaderStructure /> -->
+    <Header />
+    <main class="w-full grow">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>>
+
+<script>
+import Header from '~/components/Header/Header.vue';
+import HeaderStructure from '~/components/Header/HeaderStructure.vue';
+export default {
+  components: {
+    Header,
+    HeaderStructure
+  }
+
+}
+</script>
+
+<style>
+
+</style>
