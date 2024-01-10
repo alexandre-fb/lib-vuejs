b3abd42b2407a8aa81626a4b11e773f2edb728d4
alexandre-fb <alexandrefb.86@gmail.com>
2024-01-10 18:42:09 -0300

    feat: Add props and style

--- components/AccordionList.vue
+++ components/AccordionList.vue
@@ -1,100 +1,152 @@
 <template>
-    <div class="accordion">
-      <div v-for="(item, index) in items" :key="index">
-        <div class="accordion-item">
-          <div
-            class="accordion-title"
-            @click="handleItemClick(index)"
-            :class="{ active: isActive(index) }"
-          >
-            {{ item.title }}
-          </div>
-          <div v-if="isActive(index)" class="accordion-content">
-            {{ item.content }}
-          </div>
+  <div class="accordion">
+    <div v-for="(item, index) in items" :key="index">
+      <div 
+        class="accordion__item" 
+        :style="(border ? `border: 1px solid ${ border_color };` : '') + `border-radius: ${ rounded }px`"
+      >
+        <div
+          class="item__title"
+          @click="handleItemClick(index)"
+          :style="`background: ${ bg_title };` + (isActive(index) ? `border-radius: ${ rounded }px ${ rounded }px 0 0;` : `border-radius: ${ rounded }px;`)"
+          :aria-label="`Accordion item: ${item.title}`"
+        >
+          <h3 :style="`color: ${ title_color };`">{{ item.title }}</h3>
+          <i class="fa-solid fa-chevron-up" :style="(isActive(index) ? 'transform: rotate(180deg);' : 'transform: rotate(0);') + `color: ${ icon_color }; transition: 200ms ease-in-out;`"></i>
+        </div>
+        <div 
+          v-if="isActive(index)" 
+          class="accordion-content"
+          :style="`background: ${ bg_content }; color: ${ content_color }; border-radius: 0 0 ${ rounded }px ${ rounded }px;`"
+        >
+          {{ item.content }}
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
+
   
   <script>
-  import { ref, computed } from 'vue';
-  
-  export default {
-    props: {
-      multipleItemsOpen: {
-        type: Boolean,
-        default: false,
+import { ref } from "vue";
+
+export default {
+  props: {
+    multiple_items_open: {
+      type: Boolean,
+      default: false,
+    },
+    border: {
+      type: Boolean,
+      default: true,
+    },
+    border_color: {
+      type: String,
+      default: '#fff',
+    },
+    rounded: {
+      type: Number,
+      default: 0,
+    },
+    bg_title: {
+      type: String,
+      default: 'transparent',
+    },
+    bg_content: {
+      type: String,
+      default: 'transparent',
+    },
+    title_color: {
+      type: String,
+      default: '#000',
+    },
+    icon_color: {
+      type: String,
+      default: '#000',
+    },
+    content_color: {
+      type: String,
+      default: '#000',
+    },
+    rouded: {
+      type: Number,
+      default: 0
+    }
+  },
+  setup(props) {
+    const activeItem = ref(null);
+    const activeItems = ref([]);
+    const items = [
+      {
+        title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       },
-    },
-    setup(props) {
-      const activeItem = ref(null);
-      const items = [
-        {
-          title: 'Item 1',
-          content: 'Content for Item 1',
-        },
-        {
-          title: 'Item 2',
-          content: 'Content for Item 2',
-        },
-        {
-          title: 'Item 3',
-          content: 'Content for Item 3',
-        },
-      ];
-  
-      const isActive = (index) => {
-        if (props.multipleItemsOpen) {
-          return activeItem.value && activeItem.value.includes(index);
+      {
+        title: "Item 2",
+        content: "Content for Item 2",
+      },
+      {
+        title: "Item 3",
+        content: "Content for Item 3",
+      },
+    ];
+
+    const isActive = (index) => {
+      if (props.multiple_items_open) {
+        return activeItems.value && activeItems.value.includes(index);
+      } else {
+        return activeItem.value === index;
+      }
+    };
+
+    const handleItemClick = (index) => {
+      if (props.multiple_items_open) {
+        if (activeItems.value.includes(index)) {
+          activeItems.value = activeItems.value.filter((item) => item !== index);
+          console.log('activeItems.value', activeItems.value)
         } else {
-          return activeItem.value === index;
+          activeItems.value.push(index);
         }
-      };
-  
-      const handleItemClick = (index) => {
-        if (props.multipleItemsOpen) {
-          activeItem.value = activeItem.value ? [...activeItem.value] : [];
-          const isActive = activeItem.value.includes(index);
-          if (isActive) {
-            activeItem.value = activeItem.value.filter((item) => item !== index);
-          } else {
-            activeItem.value.push(index);
-          }
-        } else {
-          activeItem.value = isActive(index) ? null : index;
-        }
-      };
-  
-      return {
-        items,
-        isActive,
-        handleItemClick,
-      };
-    },
-  };
-  </script>
+      } else {
+        activeItem.value = isActive(index) ? null : index;
+      }
+    };
+
+    const formatedRoundedValue = (value) => {
+      if(value <= 30) {
+        return value
+      } else {
+        return 30
+      }
+    }
+
+    return {
+      items,
+      activeItems,
+      activeItem,
+      isActive,
+      handleItemClick,
+      formatedRoundedValue
+    };
+  },
+};
+</script>
   
   <style>
-  /* Estilos básicos para o acordeão */
-  .accordion-item {
-    border: 1px solid #ccc;
-    margin-bottom: 5px;
-  }
-  
-  .accordion-title {
-    padding: 10px;
-    cursor: pointer;
-    background-color: #f0f0f0;
-  }
-  
-  .accordion-title.active {
-    background-color: #e0e0e0;
-  }
-  
-  .accordion-content {
-    padding: 10px;
-    background-color: #fff;
-  }
-  </style>
-  
+/* Estilos básicos para o acordeão */
+.accordion__item {
+  margin-bottom: 5px;
+}
+
+.accordion__item .item__title {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.accordion-content {
+  padding: 10px;
+}
+</style>
--- pages/ui-components.vue
+++ pages/ui-components.vue
@@ -8,7 +8,17 @@
 
     <!-- content -->
     <section>
-      <AccordionList multipleItemsOpen />
+      <AccordionList 
+        multiple_items_open
+        :border="false"
+        border_color="#fff"
+        :rounded="15"
+        bg_title="#70b966"
+        bg_content="#6687b9"
+        title_color="#fff"
+        icon_color="#6687b9"
+        content_color="red"
+      />
     </section>
   </div>
 </template>
