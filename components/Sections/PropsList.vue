f7e848d816c434dc2d96c6446d26cdf41886906f
alexandre-fb <alexandrefb.86@gmail.com>
2024-02-11 19:06:02 -0300

    Add props

--- components/Sections/PropsList.vue
+++ components/Sections/PropsList.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="w-full">
         <ul>
-            <li v-for="(prop, index) in propsList" :key="prop.name + index" class="flex flex-col gap-1 pl-2">
+            <li v-for="(prop, index) in propsList" :key="prop.name + index" class="__prop_wrapper flex flex-col gap-1 pl-2">
                 <h4><span class="text-sm">Nome:</span> <span class="font-bold">{{ prop.name }}</span></h4>
                 <h5><span class="text-sm">Tipo:</span> <span class="font-bold">{{ prop.type }}</span></h5>
-                <h5><span class="text-sm">Obrigatória:</span> <span class="font-bold">{{ prop.required }}</span></h5>
+                <h5><span class="text-sm">Obrigatório:</span> <span class="font-bold">{{ prop.required }}</span></h5>
                 <div class="flex gap-1">
                     <h5 class="text-sm">Descrição:</h5> 
                     <p class="text-sm">{{ prop.description }}</p>
                 </div>
+
+                <!-- linha separacao -->
+                <span v-if="propsList.length !== index + 1"  class="block h-[1px] bg-primary bg-opacity-50 w-10 my-3"></span>
             </li>
         </ul>
     </div>
@@ -33,4 +36,6 @@ export default {
 };
 </script>
     
-<style scoped></style>
+<style scoped>
+
+</style>
