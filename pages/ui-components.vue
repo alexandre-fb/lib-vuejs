152b6072fa2c431178a0fc43ddddac916b3fc685
alexandre-fb <alexandrefb.86@gmail.com>
2024-02-11 23:49:03 -0300

    Feat: SectionInterativeMap

--- pages/ui-components.vue
+++ pages/ui-components.vue
@@ -2,14 +2,20 @@
   <div class="flex flex-col grow border-b-2 pb-12">
     <SectionAccordion section_id="accordion-list-section" />
   </div>
+  <div class="flex flex-col grow border-b-2 pb-12">
+    <SectionInterativeMap section_id="interative-map-section" />
+  </div>
 </template>
 
 <script>
 //sections
 import SectionAccordion from "~/components/Sections/UiElements/SectionAccordion/SectionAccordion.vue";
+import SectionInterativeMap from "~/components/Sections/UiElements/SectionInterativeMap/SectionInterativeMap.vue";
+
 export default {
   components: { 
     SectionAccordion, 
+    SectionInterativeMap
   },
   setup() {
 
