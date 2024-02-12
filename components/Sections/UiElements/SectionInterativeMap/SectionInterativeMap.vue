a25be13269b710e1b3d85517a2e9be742e607a15
alexandre-fb <alexandrefb.86@gmail.com>
2024-02-11 23:48:23 -0300

    Initial commit

---
+++ components/Sections/UiElements/SectionInterativeMap/SectionInterativeMap.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="py-10 px-3" :id="section_id">
+    <!-- Title -->
+    <SectionTitle>
+      Mapa Interativo
+    </SectionTitle>
+
+    <!-- Exemplo -->
+    <div class="mb-16">
+      <SectionSubtitle>
+        Exemplo de utilização:
+      </SectionSubtitle>
+
+      <InteraiveMap />
+    </div>
+
+    <!-- <div class="mb-16">
+      <SectionSubtitle>
+        Props:
+      </SectionSubtitle>
+
+      <PropsList :propsList="propsList" />
+    </div>
+
+    <div>
+      <SectionSubtitle>
+        Componente completo (vue 3):
+      </SectionSubtitle>
+
+      <VCodeBlock :code="componentCode" highlightjs lang="html" theme="nord" maxHeight="350px" />
+    </div> -->
+  </section>
+</template>
+    
+<script>
+import { ref } from 'vue';
+
+import VCodeBlock from '@wdns/vue-code-block';
+import InteraiveMap from '~/components/UiElements/InterativeMap/InterativeMap.vue';
+import SectionTitle from '../../SectionTitle.vue';
+import SectionSubtitle from '../../SectionSubtitle.vue';
+import PropsList from '../../PropsList.vue';
+
+import { accordionListData, propsListData, componentCodeString } from './sectionData';
+
+export default {
+  components: {
+    SectionTitle,
+    SectionSubtitle,
+    PropsList,
+    InteraiveMap,
+    VCodeBlock,
+  },
+  props: {
+    section_id: {
+      type: String,
+    }
+  },
+  setup() {
+   
+
+    return {
+
+    }
+  },
+};
+</script>
+    
+<style scoped></style>
---
+++ components/UiElements/InterativeMap/statesData.js
@@ -0,0 +1,137 @@
+export default [
+  {
+    id: "BR-AC",
+    name: "Acre",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ligula vel massa viverra rhoncus. Sed et consequat odio, nec sodales lacus. Integer fermentum ultrices purus, ut commodo tortor volutpat id. Aliquam tincidunt ante ut tortor mattis, sit amet gravida justo facilisis. Nam bibendum, dolor a gravida vestibulum, mi urna bibendum justo, ut laoreet metus tellus vel lectus. Sed ut tellus non est ullamcorper accumsan vel id quam. Aliquam erat volutpat. Curabitur vel bibendum augue, nec fermentum metus. Vestibulum accumsan tellus in libero rhoncus, at semper mi efficitur.",
+  },
+  {
+    id: "BR-AL",
+    name: "Alagoas",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id hendrerit ligula. Nulla id posuere est. Curabitur suscipit massa a dui sollicitudin, ut commodo dolor ullamcorper. Nam eu lectus ut felis tincidunt fermentum nec non purus. Nam volutpat dolor a risus auctor scelerisque. Sed euismod eleifend felis a scelerisque. Suspendisse vel libero id elit bibendum fermentum. Suspendisse ut dui eu dolor venenatis ullamcorper. Etiam ac odio vitae purus dictum consequat id ut turpis. Aliquam in augue augue. Integer ac sem ultrices, pharetra turpis et, congue dui. Vivamus sagittis est eget sapien consequat, eu fermentum libero aliquet. Suspendisse sit amet urna nec libero malesuada pharetra.",
+  },
+  {
+    id: "BR-AP",
+    name: "Amapá",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque bibendum arcu nec finibus cursus. Ut vehicula, nulla a feugiat dignissim, metus justo vulputate nunc, eu volutpat nulla sem in justo. In gravida, turpis eu euismod efficitur, dui quam aliquam urna, vitae rhoncus elit metus at lacus. Sed efficitur tellus vel commodo venenatis. In hac habitasse platea dictumst. Integer gravida a sem quis aliquam. Nunc nec interdum dolor. Integer auctor tincidunt tortor, ut blandit dolor auctor at. Nunc non felis in tortor vulputate venenatis nec in elit. Etiam id libero et quam convallis scelerisque. In hac habitasse platea dictumst.",
+  },
+  {
+    id: "BR-AM",
+    name: "Amazonas",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum commodo ullamcorper massa vitae aliquet. Fusce rhoncus, augue vel laoreet varius, turpis urna efficitur leo, a malesuada quam libero nec libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque sit amet tellus aliquet, imperdiet libero id, tincidunt turpis. Sed vitae augue non mi iaculis congue. In vulputate eros vitae lectus venenatis, eu tristique mauris gravida. Vivamus ac lacinia elit. Praesent luctus sodales nisl, vel vulputate metus rhoncus id.",
+  },
+  {
+    id: "BR-BA",
+    name: "Bahia",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum eu odio ut tincidunt. Vestibulum aliquet ex a turpis varius, non vehicula arcu pharetra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed laoreet, est vel pellentesque tincidunt, mauris elit eleifend tortor, non posuere augue elit in justo. Sed nec tortor nec nisl posuere luctus. Praesent fringilla odio at arcu pharetra, vel ultrices erat imperdiet. In dapibus dolor eu accumsan cursus. Aliquam erat volutpat.",
+  },
+  {
+    id: "BR-CE",
+    name: "Ceará",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus, risus in ullamcorper mattis, lacus eros tincidunt libero, vel dapibus justo tellus vitae orci. Curabitur ut purus vitae erat auctor vulputate. Nullam nec varius tortor. Suspendisse bibendum ligula ut ex fermentum, a fringilla lacus pharetra. Maecenas tristique mauris non vulputate fringilla. Sed quis ipsum id nisi tincidunt vehicula. Fusce nec elit leo. Suspendisse potenti. Nulla facilisi. Vivamus vulputate, purus a mattis egestas, neque metus convallis libero, ac accumsan orci ligula sit amet est.",
+  },
+  {
+    id: "BR-DF",
+    name: "Distrito Federal",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec ex id ante dictum tristique. Etiam vel semper ex. Sed nec congue odio. Sed eget neque ac arcu iaculis venenatis et nec purus. In hac habitasse platea dictumst. Nam id metus eu ligula ullamcorper viverra non vel quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce fringilla, elit sit amet malesuada ullamcorper, turpis nulla tempus libero, vel laoreet odio purus a metus. Donec rhoncus quam eu mauris ultrices aliquet. Vivamus fermentum nisl eu ante fermentum, nec fermentum augue varius. Suspendisse potenti.",
+  },
+  {
+    id: "BR-ES",
+    name: "Espírito Santo",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis justo et tellus euismod convallis. Fusce bibendum tellus sed justo imperdiet, id tristique turpis hendrerit. Sed sodales urna vel tincidunt congue. Nunc euismod luctus ex, ut aliquet elit congue vel. Integer finibus urna quis feugiat pharetra. Sed in urna urna. Etiam vel venenatis ex, vitae tincidunt elit. Vestibulum tempus libero vitae tortor sodales, ac feugiat metus lacinia. Fusce in diam aliquet, consequat metus vel, ullamcorper nunc. Etiam cursus justo velit, sit amet auctor libero egestas at.",
+  },
+  {
+    id: "BR-GO",
+    name: "Goiás",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum sagittis sem, eget euismod sapien rhoncus in. Vivamus id laoreet urna, non gravida odio. Praesent et libero et felis rhoncus fringilla in in libero. Duis lacinia justo eu nibh vulputate, vel sodales odio malesuada. Maecenas fringilla, nisl ut luctus bibendum, tellus justo congue felis, vel euismod erat purus nec sem. Praesent congue justo ac arcu cursus, vel gravida quam dapibus. Duis eu risus justo. Integer ac dolor ut odio ullamcorper tempus. Integer feugiat quam vitae cursus tincidunt.",
+  },
+  {
+    id: "BR-MA",
+    name: "Maranhão",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus euismod metus, et fermentum metus ullamcorper vel. Etiam laoreet hendrerit erat id congue. Pellentesque at elit at ligula efficitur scelerisque et eu massa. Morbi fermentum bibendum varius. Vestibulum sit amet ante hendrerit, fermentum velit et, tincidunt libero. Sed vel commodo metus. Nulla facilisi. Integer nec ex vel nisl euismod scelerisque eu sit amet dui. Integer nec laoreet orci. Maecenas facilisis dolor vel nunc dictum, non dictum sapien facilisis. Aenean euismod, nisl sit amet vestibulum elementum, dui sapien gravida lectus, sit amet auctor elit turpis et dui.",
+  },
+  {
+    id: "BR-MT",
+    name: "Mato Grosso",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut justo in arcu pellentesque luctus. Duis efficitur orci non urna pellentesque, at sollicitudin mauris pellentesque. Suspendisse sed fringilla odio, vel fermentum elit. Vestibulum sit amet urna vitae turpis varius convallis. Quisque quis lacus non nisl malesuada tincidunt vel eget arcu. Curabitur vel laoreet justo. Integer fringilla convallis ligula. Aliquam vel rhoncus justo, eget egestas purus. Aenean tristique dolor vel ultrices sagittis. Fusce eu ipsum id neque luctus bibendum. Ut nec lectus leo.",
+  },
+  {
+    id: "BR-MS",
+    name: "Mato Grosso do Sul",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vestibulum elit sed justo posuere, vel cursus elit posuere. Suspendisse bibendum tristique fermentum. Donec id ligula in urna ultrices cursus a a ante. Nam in arcu eu lectus sollicitudin luctus. Sed eu massa a justo luctus malesuada. Maecenas volutpat, orci ac scelerisque semper, justo arcu congue lacus, id volutpat sapien mi at nisi. Morbi vitae finibus risus, eu cursus mi. Donec vestibulum mauris eu mauris laoreet, vitae ultricies ex mattis. Vivamus condimentum nisl at tristique iaculis.",
+  },
+  {
+    id: "BR-MG",
+    name: "Minas Gerais",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sagittis augue id tincidunt lacinia. Ut in suscipit justo, id auctor purus. Quisque fringilla vel nisi vitae hendrerit. Fusce ultrices, libero ut blandit malesuada, sem odio eleifend justo, eget iaculis mi risus a odio. Fusce ac elit id mi hendrerit dapibus ut at justo. Sed ut elit lacus. In hac habitasse platea dictumst. Donec eu dolor massa. Aliquam erat volutpat. Suspendisse potenti. Praesent euismod, purus a varius egestas, metus orci dapibus odio, vel finibus sapien lectus ac libero. Integer a nunc ut orci cursus lacinia.",
+  },
+  {
+    id: "BR-PA",
+    name: "Pará",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel consectetur nunc, ut luctus ex. Praesent id dui volutpat, pellentesque odio nec, commodo arcu. Sed ac dui non mauris congue pharetra. Curabitur volutpat vehicula mi, id commodo nunc blandit vel. Etiam vel justo in quam ullamcorper fermentum. Morbi bibendum in justo nec tincidunt. Nunc et varius tortor. Fusce tristique, risus eu varius feugiat, turpis sem volutpat neque, a ultricies urna ex eu odio.",
+  },
+  {
+    id: "BR-PB",
+    name: "Paraíba",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce facilisis libero vel dolor aliquet efficitur. Sed vel purus id nisi fermentum commodo et vel elit. Morbi nec ligula sapien. Aliquam tincidunt velit sed nunc gravida volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin consectetur ligula vel tellus euismod fringilla. Fusce aliquam, justo quis tempus malesuada, risus ex hendrerit libero, vel interdum augue arcu ut justo. Aenean cursus fermentum orci a iaculis. Integer feugiat metus id turpis dictum, vitae auctor turpis dictum.",
+  },
+  {
+    id: "BR-PR",
+    name: "Paraná",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut massa ut lectus auctor convallis nec eu orci. Ut ac mauris in purus fermentum tincidunt. Aliquam euismod commodo libero, sit amet vehicula dui cursus nec. Nullam tristique tincidunt justo, id fermentum elit malesuada vel. Suspendisse vel quam vel dui varius malesuada nec et velit. Praesent at feugiat dui, vitae tempus arcu. Aenean ut justo ut turpis dapibus rhoncus non nec augue. Aenean et ipsum at sapien pellentesque fermentum. Duis eget sapien in mi gravida volutpat.",
+  },
+  {
+    id: "BR-PE",
+    name: "Pernambuco",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non rhoncus sem, ac ultrices libero. Aliquam euismod risus nec odio cursus dignissim. Sed sit amet augue vel libero auctor laoreet. Sed eu urna quam. Fusce eu efficitur elit. Fusce hendrerit, felis at pharetra tincidunt, justo purus mattis purus, eu tempus nulla urna vel dolor. Vestibulum euismod bibendum mi, vel luctus libero vulputate vel. Fusce eu turpis non ligula ultrices dapibus sit amet at urna.",
+  },
+  {
+    id: "BR-PI",
+    name: "Piauí",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu justo eu nulla ultricies imperdiet eu a dolor. Curabitur ultrices nulla et accumsan hendrerit. Sed auctor, orci in luctus cursus, dolor est tincidunt purus, id pharetra purus libero ut lectus. Donec non dictum metus, ut fermentum libero. Duis scelerisque imperdiet efficitur. Aliquam erat volutpat. Donec ut erat odio. Fusce eu mi quis mi interdum fringilla. Morbi ac massa eget velit efficitur semper. Proin pharetra, libero nec ultrices suscipit, sem eros tristique orci, vel scelerisque justo mauris in dui.",
+  },
+  {
+    id: "BR-RJ",
+    name: "Rio de Janeiro",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia, mi in iaculis tincidunt, augue massa convallis mi, a tristique massa augue quis urna. Nulla vestibulum risus eu elit vulputate, vel cursus elit ultricies. Proin laoreet scelerisque neque. Etiam ac cursus purus. Vestibulum condimentum massa ut ligula sollicitudin, ac ultrices est malesuada. Nulla facilisi. Sed sodales, metus nec fermentum ultricies, nulla arcu blandit purus, ut bibendum lectus leo vel libero. Aliquam vel malesuada orci. Nulla facilisi.",
+  },
+  {
+    id: "BR-RN",
+    name: "Rio Grande do Norte",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu purus ut ante aliquam feugiat. Duis sed odio bibendum, varius lacus id, egestas ligula. Fusce semper, libero et congue ultricies, lectus libero tristique nisl, ut tincidunt nisi libero nec ex. Vestibulum et lacus vel justo cursus tempor. Vivamus efficitur lectus nec urna malesuada aliquam. Vestibulum fringilla orci ut nibh auctor ultricies. Vivamus nec libero sapien. Ut bibendum rhoncus risus a efficitur. In hac habitasse platea dictumst.",
+  },
+  {
+    id: "BR-RS",
+    name: "Rio Grande do Sul",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu purus nec justo pulvinar pellentesque. Vivamus vel purus efficitur, fermentum quam ac, tempus purus. Suspendisse vel metus sed elit tristique egestas. Nulla facilisi. Nam posuere fermentum venenatis. Integer eget nunc vel metus iaculis congue. Fusce eu nisi vel urna varius commodo id ut sem. Ut sed felis ligula. Vivamus finibus bibendum nunc, in varius arcu. Nullam suscipit, libero a gravida accumsan, erat purus sodales purus, in lacinia sem velit at turpis. Praesent suscipit sit amet risus in tempor. Curabitur dapibus vel velit ut sodales. Quisque auctor vestibulum ipsum eu tristique. Fusce vel turpis eu dui tincidunt bibendum eu non est.",
+  },
+  {
+    id: "BR-RO",
+    name: "Rondônia",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec mi nunc. Suspendisse potenti. Fusce et leo tristique, fringilla lacus ac, cursus sapien. Etiam a mi ut tortor efficitur vulputate vel eu est. Integer eu mauris sit amet elit euismod iaculis. Maecenas congue metus vel dapibus viverra. Duis ultrices ac orci a cursus. Sed eget facilisis augue, vel interdum elit. Ut vehicula quam in mauris pharetra, non congue leo gravida. Praesent eget libero vel quam pharetra tincidunt. Nullam finibus vestibulum lacus, vel volutpat libero facilisis et. Ut eu augue accumsan, sollicitudin turpis ut, hendrerit ligula. Quisque tincidunt vehicula ex, vel vehicula ex accumsan eget. Sed facilisis, libero ac aliquet viverra, ante elit laoreet libero, ut cursus sapien elit vel turpis. In hac habitasse platea dictumst.",
+  },
+  {
+    id: "BR-RR",
+    name: "Roraima",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel tortor vel urna varius posuere. Sed et nisi sed libero fermentum scelerisque. Nunc vitae erat vel elit blandit eleifend non vel nulla. In dapibus dolor vel justo varius, ut luctus neque tincidunt. Duis non efficitur dui. Curabitur sit amet sem cursus, iaculis massa eu, iaculis metus. In nec risus urna. Vivamus sit amet nisl eget justo sagittis dignissim. Praesent eu tincidunt tellus. Quisque eu justo fermentum, luctus neque vel, varius arcu. Morbi eu tortor id lacus semper ullamcorper. Fusce laoreet euismod rhoncus. Maecenas tincidunt, elit eu pellentesque fermentum, purus turpis vestibulum sem, ac consectetur elit nisl eu ligula.",
+  },
+  {
+    id: "BR-SC",
+    name: "Santa Catarina",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet lectus ut accumsan efficitur. Nulla facilisi. Nullam auctor ultricies libero, non cursus arcu dictum a. Nulla facilisi. Nunc sed sem id nisi laoreet vestibulum a vitae metus. Vestibulum efficitur nibh eu libero consectetur, ut bibendum velit pulvinar. Ut pellentesque ut sem sit amet cursus. Nunc pharetra neque a ante hendrerit egestas. Nam quis arcu ultrices, varius velit id, semper metus. Vestibulum fringilla, elit at dictum commodo, ex velit sollicitudin libero, in venenatis orci lacus eu sem. Curabitur vestibulum mi eget mi fermentum, ut fermentum arcu malesuada. Vestibulum facilisis urna sit amet justo tincidunt, nec euismod metus pulvinar. Nam imperdiet ex at magna consequat, non suscipit quam vulputate.",
+  },
+  {
+    id: "BR-SP",
+    name: "São Paulo",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rhoncus tempus lorem, nec aliquam nisl sollicitudin ut. Aliquam interdum lacus non arcu imperdiet vestibulum. Vivamus interdum aliquet arcu vel ultricies. Proin in finibus sapien. Curabitur malesuada consectetur purus, sit amet scelerisque lectus facilisis eu. Vivamus eu facilisis augue. Quisque eu ligula ut mauris tristique blandit. Fusce vel varius neque, nec dapibus felis. Quisque venenatis efficitur felis, ut laoreet arcu. Nulla facilisi. Nunc vitae elit nec risus vehicula malesuada ut nec ex.",
+  },
+  {
+    id: "BR-SE",
+    name: "Sergipe",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non dolor auctor, vehicula justo at, pellentesque nisl. Duis at justo in purus suscipit tristique nec eget urna. Etiam malesuada tincidunt venenatis. Curabitur facilisis turpis a enim gravida, a volutpat ligula bibendum. Suspendisse potenti. Integer pellentesque malesuada felis eu lacinia. Sed et mauris vel libero consequat tincidunt. Nunc at enim vel nisi aliquet ultrices vitae nec enim. Quisque tincidunt, nulla a aliquet fermentum, ligula turpis vehicula dolor, ut cursus nunc sem in libero. Integer vel magna tristique, ultrices odio id, efficitur tellus. Morbi hendrerit purus vitae dui finibus, at scelerisque dui congue. In hac habitasse platea dictumst.",
+  },
+  {
+    id: "BR-TO",
+    name: "Tocantins",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus mi quis leo pulvinar scelerisque. Nunc consectetur vehicula turpis, eget cursus nisl venenatis nec. Donec vestibulum elit eu arcu vehicula, sit amet fringilla justo ultrices. Aenean id consectetur nulla. Maecenas ullamcorper lectus non gravida volutpat. Duis nec nunc id felis tincidunt condimentum vel at justo. In hac habitasse platea dictumst. Sed imperdiet arcu nec lacus lacinia, sit amet suscipit ligula rhoncus. Proin at efficitur quam, vel sagittis libero. Ut bibendum, sapien non vestibulum lacinia, mi neque maximus dui, vel convallis neque sapien vitae libero. Quisque euismod purus nec efficitur varius. Integer pharetra sodales libero, vel feugiat metus scelerisque at.",
+  },
+];
