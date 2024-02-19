648a6c4d18454692af7c6f753ecb0baaa9bf0812
alexandre-fb <alexandrefb.86@gmail.com>
2024-02-18 23:41:50 -0300

    fix: change heaader to div

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col h-full min-h-screen">
     <!-- <HeaderStructure /> -->
-    <header>
+    <div>
       <Header />
-    </header>
+    </div>
 
     <div class="flex w-full max-w-container grow mx-auto">
       <aside>
